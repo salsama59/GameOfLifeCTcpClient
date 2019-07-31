47f4552b1d42b94ee5f3988f2c2ffd07aa3baa1c
YEPONDE Saül <syeponde@partner.oney.fr>
2019-07-31 17:42:15 +0200

    Fix dependencies

--- src/main/java/com/internal/gameoflife/client/TCPClient.java
+++ src/main/java/com/internal/gameoflife/client/TCPClient.java
@@ -3,16 +3,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-class TCPClient {
+/**
+ * This class represent the tcp client.
+ * It purpose is to read from the socket input stream in order to display the simulation data.
+ * @author syeponde
+ */
+public class TCPClient {
 	public static void main(String argv[]) throws Exception {
 		while(true) {
-			Socket clientSocket = new Socket("localhost", 4444);
-			if(!clientSocket.isInputShutdown()) {
-				InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
-				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-				String serverMessage = null;
-				while((serverMessage = bufferedReader.readLine()) != null) {
-					System.out.println(serverMessage);
+			try (Socket clientSocket = new Socket("localhost", 4444)) {
+				if(!clientSocket.isInputShutdown()) {
+					InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+					BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+					String serverMessage = null;
+					while((serverMessage = bufferedReader.readLine()) != null) {
+						System.out.println(serverMessage);
+					}
 				}
 			}
 		}
